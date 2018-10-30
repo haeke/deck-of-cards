9be712c3f44402dd5579de3e7ed2ebc10b016308
waehake <erivera@student.wpunj.edu>
2018-10-29 21:22:29 -0400

    Updates

---
+++ precache-manifest.0839eb8d1bf5299350414bc619363cc3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f317f0df2629cb3a97ae",
+    "url": "/deck-of-cards/static/js/runtime~main.f317f0df.js"
+  },
+  {
+    "revision": "050fd52fc4773273ab6e",
+    "url": "/deck-of-cards/static/js/main.050fd52f.chunk.js"
+  },
+  {
+    "revision": "707d06180b9a96fa3ba8",
+    "url": "/deck-of-cards/static/js/1.707d0618.chunk.js"
+  },
+  {
+    "revision": "050fd52fc4773273ab6e",
+    "url": "/deck-of-cards/static/css/main.f902b3fa.chunk.css"
+  },
+  {
+    "revision": "5d16b37f2350a5277725d354417615c3",
+    "url": "/deck-of-cards/index.html"
+  }
+];
