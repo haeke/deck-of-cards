ec5ec8d8152470be99433adffb1c9a55430492de
Edwin Rivera <waehake@gmail.com>
2018-10-31 12:38:00 -0400

    Updates

---
+++ precache-manifest.563b4575e7d308373ad5b324367ecaa5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f317f0df2629cb3a97ae",
+    "url": "/deck-of-cards/static/js/runtime~main.f317f0df.js"
+  },
+  {
+    "revision": "207a4fd0932b1184ed43",
+    "url": "/deck-of-cards/static/js/main.207a4fd0.chunk.js"
+  },
+  {
+    "revision": "a5222c692d2b7a79876b",
+    "url": "/deck-of-cards/static/js/1.a5222c69.chunk.js"
+  },
+  {
+    "revision": "207a4fd0932b1184ed43",
+    "url": "/deck-of-cards/static/css/main.bffa530e.chunk.css"
+  },
+  {
+    "revision": "2acd4a39aff78e0984df7ca07fc2171b",
+    "url": "/deck-of-cards/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/deck-of-cards/precache-manifest.4382f19105734c49abd2024a4b5a80c9.js"
+  "/deck-of-cards/precache-manifest.563b4575e7d308373ad5b324367ecaa5.js"
 );
 
 workbox.clientsClaim();
